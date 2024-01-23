4d4018868a5ecda08aa956dc9edb79612182dfa9
DreadDev1 <kameen.avery@gmail.com>
2024-01-23 16:26:08 -0500

    Section 2 - Project Creation 15 Post Process Highlight

---
+++ Source/Aura/Public/Characters/Enemies/AuraEnemy.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/Base/BaseCharacter.h"
+#include "Framework/Interaction/EnemyInterface.h"
+#include "AuraEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraEnemy : public ABaseCharacter, public IEnemyInterface
+{
+	GENERATED_BODY()
+public:
+	AAuraEnemy();
+	UPROPERTY(BlueprintReadOnly)
+	bool bHighlighted = false;
+
+	/** Enemy Interface */
+	virtual void HighlightActor() override;
+	virtual void UnHighlightActor() override;
+	/** End Enemy Interface */
+};
