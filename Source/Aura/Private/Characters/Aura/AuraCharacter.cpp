d5183b7796a409bd04d41734d701a159b573a2db
DreadDev <kavery@live.com>
2024-01-26 23:05:53 -0500

    Section 5 - RPG Game UI 33 Overlay Widget Controller

--- Source/Aura/Private/Characters/Aura/AuraCharacter.cpp
+++ Source/Aura/Private/Characters/Aura/AuraCharacter.cpp
@@ -8,7 +8,9 @@
 #include "GameFramework/SpringArmComponent.h"
 
 #include "AbilitySystemComponent.h"
+#include "Player/AuraPlayerController.h"
 #include "Player/AuraPlayerState.h"
+#include "UI/AuraHUD.h"
 
 AAuraCharacter::AAuraCharacter()
 {
@@ -57,4 +59,12 @@ void AAuraCharacter::InitAbilityActorInfo()
 	AuraPlayerState->GetAbilitySystemComponent()->InitAbilityActorInfo(AuraPlayerState, this);
 	AbilitySystemComponent = AuraPlayerState->GetAbilitySystemComponent();
 	AttributeSet = AuraPlayerState->GetAttributeSet();
+
+	if (AAuraPlayerController* AuraPlayerController = Cast<AAuraPlayerController>(GetController()))
+	{
+		if (AAuraHUD* AuraHUD = Cast<AAuraHUD>(AuraPlayerController->GetHUD()))
+		{
+			AuraHUD->InitOverlay(AuraPlayerController, AuraPlayerState, AbilitySystemComponent, AttributeSet);
+		}
+	}
 }
