b95a9140593f64ae4abc893a65865a8f50464af0
DreadDev <kavery@live.com>
2024-02-10 03:36:39 -0500

    Section 9 - Attribute Menu 89 Native Gameplay Tags

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -8,5 +8,43 @@ FAuraGameplayTags FAuraGameplayTags::GameplayTags;
 
 void FAuraGameplayTags::InitializeNativeGameplayTags()
 {
-	GameplayTags.Attributes_Secondary_Armor = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.Armor"), FString("Reduces damage taken, improves Block Chance"));
+#pragma region Primary Attributes 
+	GameplayTags.Attributes_Primary_Strength = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("Attributes.Primary.Strength"), FString("Increases Physical Damage"));
+	GameplayTags.Attributes_Primary_Intelligence = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("Attributes.Primary.Intelligence"), FString("Increases Magical Damage"));
+	GameplayTags.Attributes_Primary_Resilience = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("Attributes.Primary.Resilience"), FString("Increases Armor and Armor Penetration"));
+	GameplayTags.Attributes_Primary_Vigor = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("Attributes.Primary.Vigor"), FString("Increases Heath"));
+#pragma endregion 
+#pragma region Secondary Attributes 
+	GameplayTags.Attributes_Secondary_Armor = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("Attributes.Secondary.Armor"), FString("Reduce Damage taken improves Block Chance"));
+	GameplayTags.Attributes_Secondary_ArmorPenetration = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("Attributes.Secondary.ArmorPenetration"), FString("Ignores Percentage of enemy Armor, increases Crit Hit Chance"));
+	GameplayTags.Attributes_Secondary_BlockChance = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("Attributes.Secondary.BlockChance"), FString("Chance to cut incoming damage in half"));
+	GameplayTags.Attributes_Secondary_CritHitChance = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("Attributes.Secondary.CritHitChance"), FString("Chance to double damage plus critical hit bonus"));
+	GameplayTags.Attributes_Secondary_CritHitDamage = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("Attributes.Secondary.CritHitDamage"), FString("Bonus damage added when Crit Hit is scored"));
+	GameplayTags.Attributes_Secondary_CritHitResist = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("Attributes.Secondary.CritHitResist"), FString("Reduce Crit Hit Damage of attacking enemies"));
+	GameplayTags.Attributes_Secondary_HealthRegen = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("Attributes.Secondary.HealthRegen"), FString("Amount of Health regenerated every 1 second"));
+	GameplayTags.Attributes_Secondary_ManaRegen = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("Attributes.Secondary.ManaRegen"), FString("Amount of Mana regenerated every 1 second"));
+	GameplayTags.Attributes_Secondary_MaxHealth = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("Attributes.Secondary.MaxHealth"), FString("Max Health for Player"));
+	GameplayTags.Attributes_Secondary_MaxMana = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("Attributes.Secondary.MaxMana"), FString("Max Mana for Player"));
+#pragma endregion
+
+#pragma region Primary Attributes 
+	GameplayTags.Attributes_Vital_Health = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("Attributes.Vital.Health"), FString("Current Player Health"));
+	GameplayTags.Attributes_Vital_Mana = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("Attributes.Vital.Mana"), FString("Current Player Mana"));
+#pragma endregion
 }
--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -6,8 +6,6 @@
 #include "GameplayTagContainer.h"
 
 /**
- * AuraGameplayTags
- *
  * Singleton containing native Gameplay Tags
  */
 
@@ -17,7 +15,31 @@ public:
 	static const FAuraGameplayTags& Get() { return GameplayTags;}
 	static void InitializeNativeGameplayTags();
 
+#pragma region Primary Attributes 
+	FGameplayTag Attributes_Primary_Strength;
+	FGameplayTag Attributes_Primary_Intelligence;
+	FGameplayTag Attributes_Primary_Resilience;
+	FGameplayTag Attributes_Primary_Vigor;
+#pragma endregion
+	
+#pragma region Secondary Attributes 
 	FGameplayTag Attributes_Secondary_Armor;
+	FGameplayTag Attributes_Secondary_ArmorPenetration;
+	FGameplayTag Attributes_Secondary_BlockChance;
+	FGameplayTag Attributes_Secondary_CritHitChance;
+	FGameplayTag Attributes_Secondary_CritHitDamage;
+	FGameplayTag Attributes_Secondary_CritHitResist;
+	FGameplayTag Attributes_Secondary_HealthRegen;
+	FGameplayTag Attributes_Secondary_ManaRegen;
+	FGameplayTag Attributes_Secondary_MaxHealth;
+	FGameplayTag Attributes_Secondary_MaxMana;
+#pragma endregion
+
+#pragma region Vital Attributes 
+	FGameplayTag Attributes_Vital_Health;
+	FGameplayTag Attributes_Vital_Mana;
+#pragma endregion
+	
 protected:
 
 private:
