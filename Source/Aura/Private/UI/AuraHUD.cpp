49bc16cad3634d6caa5f82e6bece4b4653f73305
DreadDev <kavery@live.com>
2024-02-11 03:10:40 -0500

    Section 9 - Attribute Menu 91 Aura Ability System Blueprint Library

--- Source/Aura/Private/UI/AuraHUD.cpp
+++ Source/Aura/Private/UI/AuraHUD.cpp
@@ -12,7 +12,6 @@ UOverlayWidgetController* AAuraHUD::GetOverlayWidgetController(const FWidgetCont
 	{
 		OverlayWidgetController = NewObject<UOverlayWidgetController>(this, OverlayWidgetControllerClass);
 		OverlayWidgetController->SetWidgetControllerParams(WCParams);
-
 		OverlayWidgetController->BindCallbacksToDependencies();
 		return OverlayWidgetController;
 	}
